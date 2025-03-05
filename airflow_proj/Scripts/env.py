7ca25fd37769d0bbb1a6dff126ee56099164d813
vijay <vijay6301@gmail.com>
2025-03-05 08:55:32 +0000

    env_variables

--- airflow_proj/Scripts/env.py
+++ airflow_proj/Scripts/env.py
@@ -4,12 +4,12 @@ import sys
 current_dir =   os.getcwd()
 
 
-account='mn92153.ap-southeast-1'
-user='vijay6301'
-password='Vijay.snow@630154'
-database='PANDAS'
-schema='RAW_LAYER'
-warehouse='COMPUTE_WH'
-role='ACCOUNTADMIN'
+account=''
+user=''
+password=''
+database=''
+schema=''
+warehouse=''
+role=''
 
 file_dirs= current_dir +'\\airflow_proj\\src\\*'
